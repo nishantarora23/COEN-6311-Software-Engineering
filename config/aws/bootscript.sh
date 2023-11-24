2bd2956cc00c4ba1f92089d6157982ae43a6c089
Nishant Arora <aroranish23@gmail.com>
2023-11-24 11:49:20 -0500

    AWS configuration files

---
+++ config/aws/bootscript.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install Git
+sudo yum install git -y
+git clone https://github.com/nishantarora23/ICDE-JobHive.git
+
+# Install Docker
+sudo yum update -y
+sudo yum install -y docker
+
+# Start Docker
+sudo service docker start
+sudo systemctl enable docker
+
+# Wait for Docker to start
+sleep 10
+
+# Add ec2-user to docker group
+sudo usermod -aG docker ec2-user
+
+# Configure and Install Docker-Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
---
+++ config/aws/replace.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Check if a new IP address is provided as an argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <new_ip_address>"
+    exit 1
+fi
+
+# New IP address to replace
+new_ip="$1"
+
+# List of files to modify
+files=("/home/ec2-user/ICDE-JobHive/web/src/constants.ts", "/home/ec2-user/ICDE-JobHive/src/main/java/Helper/Helper.java", "")
+
+# IP address to replace
+old_ip="localhost"
+
+# Loop through each file and replace the IP address
+for file in "${files[@]}"; do
+    # Check if the file exists
+    if [ -f "$file" ]; then
+        # Replace the IP address using sed
+        sed -i "s/$old_ip/$new_ip/g" "$file"
+        echo "Replaced $old_ip with $new_ip in $file"
+    else
+        echo "File $file does not exist."
+    fi
+done
