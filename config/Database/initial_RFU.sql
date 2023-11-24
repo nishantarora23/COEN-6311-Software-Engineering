e920158de852aa9aa102113a806a243e27159393
merlin <merlinabraham95@gmail.com>
2023-11-24 17:06:14 -0500

    changes

--- config/Database/initial_RFU.sql
+++ config/Database/initial_RFU.sql
@@ -1,28 +1,45 @@
-CREATE DATABASE IF NOT EXISTS COEN6311;
+-- Drop the existing database if it exists
+DROP DATABASE IF EXISTS `COEN6311`;
 
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS `COEN6311`;
+
+-- Create a new `USERS` table with the desired structure
 CREATE TABLE `COEN6311`.`USERS` (
-  ID int NOT NULL AUTO_INCREMENT,
-  FULLNAME varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  USERNAME varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PASSWORD varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  EMAIL varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  ADDRESS varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  DOB varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL, -- Change data type to VARCHAR
-  CITY varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PROVINCE varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  COUNTRY varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (ID)
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `FULLNAME` VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `USERNAME` VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `PASSWORD` VARCHAR(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `EMAIL` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `ADDRESS` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `DOB` VARCHAR(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL, -- Change data type to VARCHAR
+  `CITY` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `PROVINCE` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `COUNTRY` VARCHAR(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`ID`)
 );
 
-INSERT INTO `COEN6311`.`USERS` (FULLNAME, USERNAME, PASSWORD, EMAIL, ADDRESS, DOB, CITY, PROVINCE, COUNTRY) VALUES
+-- Insert dummy data into the `USERS` table
+INSERT INTO `COEN6311`.`USERS` (`FULLNAME`, `USERNAME`, `PASSWORD`, `EMAIL`, `ADDRESS`, `DOB`, `CITY`, `PROVINCE`, `COUNTRY`) VALUES
 ('John Doe', 'johndoe', 'password1', 'johndoe@example.com', '123 Main St', '1990-01-15', 'New York', 'NY', 'USA'),
 ('Jane Smith', 'janesmith', 'password2', 'janesmith@example.com', '456 Elm St', '1985-04-20', 'Los Angeles', 'CA', 'USA'),
 ('Alice Johnson', 'alicej', 'password3', 'alice@example.com', '789 Oak St', '2000-09-10', 'Chicago', 'IL', 'USA');
 
+-- Create a new table `PROFILES` for data capture of user skills and location
 CREATE TABLE `COEN6311`.`PROFILES` (
-  `ID` int NOT NULL,
-  `SKILLS` varchar(255) COLLATE utf8mb4_unicode_ci,
-  `LOCATION` varchar(255) COLLATE utf8mb4_unicode_ci,
-  `DESIRED_ROLE` varchar(255) COLLATE utf8mb4_unicode_ci,
+  `ID` INT NOT NULL,
+  `SKILLS` VARCHAR(255) COLLATE utf8mb4_unicode_ci,
+  `LOCATION` VARCHAR(255) COLLATE utf8mb4_unicode_ci,
+  `DESIRED_ROLE` VARCHAR(255) COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (`ID`)
+);
+
+-- Create a new table `SEARCH` for data capture of user search history
+CREATE TABLE `COEN6311`.`SEARCH` (
+    `SEARCH_ID` INT AUTO_INCREMENT, -- This column will auto-increment
+    `USER_ID` INT,
+	`SEARCH_KEYWORD` VARCHAR(255),
+    `SEARCH_LOCATION` VARCHAR(255),
+    `SEARCH_TIMESTAMP` TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Optional: a timestamp for when the search was performed
+    PRIMARY KEY (`SEARCH_ID`)
 );
