3cd743f43639b0b026b1c49b400d5901642118af
merlin <merlinabraham95@gmail.com>
2023-12-03 15:50:41 -0500

    nginx config fix for resume builder

--- webapp/resume/vite.config.js
+++ webapp/resume/vite.config.js
@@ -1,22 +1,17 @@
 import { defineConfig } from "vite";
-
 import Unocss from "unocss/vite";
-
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
   server: {
-    port: 3001
+    port: 3001,
   },
   preview: {
-    port: 8000
+    port: 8000,
   },
-  plugins: [
-    Unocss(),
-    solidPlugin(),
-  ],
+  base : "/resume/",
+  plugins: [Unocss(), solidPlugin()],
   build: {
     target: "esnext",
   },
-  base: "/resume/",
 });
